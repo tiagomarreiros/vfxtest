8f233289c0417711021ac301399b104ad171b7a3
Tiago Marreiros <tiagocrispim.m@gmail.com>
2024-05-06 23:06:32 +0100

    remove global error handling

--- src/api/alphaApi.ts
+++ src/api/alphaApi.ts
@@ -1,18 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { DataMonthlyResponse, SearchTicketResponse } from '../types/Ticker';
 import { ExchangeRateResponse } from '../types/ExchangeRate';
-import { Alert } from 'react-native';
 
-
-const onQueryStartedErrorToast = async ({ queryFulfilled }) => {
-  try {
-    await queryFulfilled;
-  } catch (error) {
-    Alert.alert('Error', 'Error getting data', [
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
-  }
-};
 
 export const api = createApi({
   reducerPath: 'alphaApi',
@@ -20,15 +9,12 @@ export const api = createApi({
   endpoints: (builder) => ({
     getTickersByKeywords: builder.query<SearchTicketResponse, string | undefined>({
       query: (keywords) => `query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${process.env.API_KEY}`,
-      onQueryStarted: onQueryStartedErrorToast,
     }),
     getDataMonthly: builder.query<DataMonthlyResponse, string | undefined>({
       query: (symbol) => `query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${process.env.API_KEY}`,
-      onQueryStarted: onQueryStartedErrorToast,
     }),
     getExchangeForPair : builder.query<ExchangeRateResponse, {period?: string, from?: string, to?: string}>({
       query: ({period, from, to}) => `query?function=${period}&from_symbol=${from}&to_symbol=${to}&apikey=${process.env.API_KEY}`,
-      onQueryStarted: onQueryStartedErrorToast,
     }),
   }),
 
